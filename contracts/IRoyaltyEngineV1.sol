13a4a0771cd294d2459c45eff0e329591a973d3f
Wilkins Chung <wilkins.chung@gmail.com>
2021-09-18 13:36:33 -0700

    add royalty engine implementation

---
+++ contracts/IRoyaltyEngineV1.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/// @author: manifold.xyz
+
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+/**
+ * @dev Lookup engine interface
+ */
+interface IRoyaltyEngineV1 is IERC165 {
+
+    /**
+     * Get the royalty for a given token (address, id) and value amount.  Never cache the result, as the recipients and values can change.
+     * 
+     * @param tokenAddress - The address of the token
+     * @param tokenId      - The id of the token
+     * @param value        - The value you wish to get the royalty of
+     *
+     * returns Two arrays of equal length, royalty recipients and the corresponding amount each recipient should get
+     */
+    function getRoyalty(address tokenAddress, uint256 tokenId, uint256 value) external view returns(address payable[] memory recipients, uint256[] memory amounts);
+}
---
+++ contracts/RoyaltyEngineV1.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/// @author: manifold.xyz
+
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+
+import "./specs/IManifold.sol";
+import "./specs/IRarible.sol";
+import "./specs/IFoundation.sol";
+import "./specs/IEIP2981.sol";
+import "./specs/IZoraOverride.sol";
+import "./IRoyaltyEngineV1.sol";
+import "./IRoyaltyRegistry.sol";
+
+/**
+ * @dev Engine to lookup royalty configurations
+ */
+contract RoyaltyEngineV1 is ERC165, OwnableUpgradeable, IRoyaltyEngineV1 {
+    using AddressUpgradeable for address;
+
+    address public royaltyRegistry;
+
+    function initialize(address royaltyRegistry_) public initializer {
+        __Ownable_init_unchained();
+        require(ERC165Checker.supportsInterface(royaltyRegistry_, type(IRoyaltyRegistry).interfaceId));
+        royaltyRegistry = royaltyRegistry_;
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
+        return interfaceId == type(IRoyaltyEngineV1).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev See {IRegistry-getRoyalty}.
+     */
+    function getRoyalty(address tokenAddress, uint256 tokenId, uint256 value) public view override returns(address payable[] memory recipients, uint256[] memory amounts) {
+        address originalTokenAddress = tokenAddress;
+        address overrideAddress = IRoyaltyRegistry(royaltyRegistry).getOverride(tokenAddress);
+
+        // Use override if configured
+        if (overrideAddress != address(0)) tokenAddress = overrideAddress;
+
+        if (ERC165Checker.supportsInterface(tokenAddress, type(IManifold).interfaceId)) {
+            // Supports manifold interface.  Compute amounts
+            uint256[] memory bps;
+            (recipients, bps) = IManifold(tokenAddress).getRoyalties(tokenId);
+            require(recipients.length == bps.length);
+            return (recipients, _computeAmounts(value, bps));
+        } else if (ERC165Checker.supportsInterface(tokenAddress, type(IRaribleV2).interfaceId)) {
+            // Supports rarible v2 interface. Compute amounts
+            IRaribleV2.Part[] memory royalties = IRaribleV2(tokenAddress).getRaribleV2Royalties(tokenId);
+            recipients = new address payable[](royalties.length);
+            amounts = new uint256[](royalties.length);
+            for (uint i = 0; i < royalties.length; i++) {
+                recipients[i] = royalties[i].account;
+                amounts[i] = value*royalties[i].value/10000;
+            }
+            return (recipients, amounts);
+        } else if (ERC165Checker.supportsInterface(tokenAddress, type(IRaribleV1).interfaceId)) {
+            // Supports rarible v1 interface. Compute amounts
+            recipients = IRaribleV1(tokenAddress).getFeeRecipients(tokenId);
+            uint256[] memory bps = IRaribleV1(tokenAddress).getFeeBps(tokenId);
+            require(recipients.length == bps.length);
+            return (recipients, _computeAmounts(value, bps));
+        } else if (ERC165Checker.supportsInterface(tokenAddress, type(IFoundation).interfaceId)) {
+            // Supports foundation interface.  Compute amounts
+            uint256[] memory bps;
+            (recipients, bps) = IFoundation(tokenAddress).getFees(tokenId);
+            require(recipients.length == bps.length);
+            return (recipients, _computeAmounts(value, bps));
+        } else if (ERC165Checker.supportsInterface(tokenAddress, type(IEIP2981).interfaceId)) {
+            // Supports EIP2981.  Return amounts
+            (address recipient, uint256 amount) = IEIP2981(tokenAddress).royaltyInfo(tokenId, value);
+            recipients = new address payable[](1);
+            amounts = new uint256[](1);
+            recipients[0] = payable(recipient);
+            amounts[0] = amount;
+        } else if (ERC165Checker.supportsInterface(tokenAddress, type(IZoraOverride).interfaceId)) {
+            // Support Zora override
+            uint256[] memory bps;
+            (recipients, bps) = IZoraOverride(tokenAddress).convertBidShares(originalTokenAddress, tokenId);
+            return (recipients, _computeAmounts(value, bps));
+        }
+        return (recipients, amounts);
+    }
+
+    /**
+     * Compute royalty amounts
+     */
+    function _computeAmounts(uint256 value, uint256[] memory bps) private pure returns(uint256[] memory amounts) {
+        amounts = new uint256[](bps.length);
+        for (uint i = 0; i < bps.length; i++) {
+            amounts[i] = value*bps[i]/10000;
+        }
+        return amounts;
+    }
+
+
+}
