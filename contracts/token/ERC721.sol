2a2277295680193584a08b610aa72119ca62718a
Wilkins Chung <wilkins.chung@gmail.com>
2021-10-22 06:47:55 -0700

    move token implementation directory

--- contracts/token/ERC721.sol
+++ contracts/token/ERC721.sol
@@ -5,19 +5,19 @@ pragma solidity ^0.8.0;
 /// @author: manifold.xyz
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-import "../RoyaltyOverrideCore.sol";
+import "../overrides/RoyaltyOverrideCore.sol";
 
 /**
- * Reference implementation of ERC1155 with EIP2981 support
+ * Reference implementation of ERC721 with EIP2981 support
  */
-contract ERC1155WithEIP2981 is ERC1155, EIP2981RoyaltyOverrideCore, Ownable {
+contract ERC721WithEIP2981 is ERC721, EIP2981RoyaltyOverrideCore, Ownable {
 
-    constructor(string memory uri_) ERC1155(uri_) {}
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, EIP2981RoyaltyOverrideCore) returns (bool) {
-        return ERC1155.supportsInterface(interfaceId) || EIP2981RoyaltyOverrideCore.supportsInterface(interfaceId);
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, EIP2981RoyaltyOverrideCore) returns (bool) {
+        return ERC721.supportsInterface(interfaceId) || EIP2981RoyaltyOverrideCore.supportsInterface(interfaceId);
     }
 
     /**
