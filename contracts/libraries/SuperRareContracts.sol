66b4e6f063e66e49ed6d077dd58f8ad7fe30aaef
koloz <zach.kolodny@gmail.com>
2022-02-02 16:46:47 -0500

    updated superrare ropsten addresses

--- contracts/libraries/SuperRareContracts.sol
+++ contracts/libraries/SuperRareContracts.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.0;
 
 library SuperRareContracts {
-    address public constant SUPERRARE_REGISTRY = 0x17B0C8564E53f22364A6C8de6F7ca5CE9BEa4e5D;
-    address public constant SUPERRARE_V1 = 0x41A322b28D0fF354040e2CbC676F0320d8c8850d;
-    address public constant SUPERRARE_V2 = 0xb932a70A57673d89f4acfFBE830E8ed7f75Fb9e0;
+    address public constant SUPERRARE_REGISTRY = 0xEBeF625cca3C0B01E3e428e8c4684d82b366B501;
+    address public constant SUPERRARE_V1 = 0xA42E14b40BB22bc3dAaf8ecAD9d73BDF44056959;
+    address public constant SUPERRARE_V2 = 0x84691657FD6Bcf50764D9FEf2a53b22c8Bd2202E;
 
 }
