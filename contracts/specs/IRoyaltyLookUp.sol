81d29e13bcfab370cc538996d1c3ede7eb35e367
Tony Snark <111127519+newts6@users.noreply.github.com>
2022-11-25 14:02:09 +0400

    refactor: naming constistency

--- contracts/specs/IRoyaltyLookUp.sol
+++ contracts/specs/IRoyaltyLookUp.sol
@@ -14,10 +14,10 @@ interface IRoyaltyLookUp {
      * @param tokenAddress Token for which to retrieve the royalty
      * @param tokenId Individual Token Id for which to retrieve the royalty. This is optional.
      * @return recipients List of recipients for the royalty. If empty, no royalty is set.
-     * @return feeInBPS List of fees express in bps to send to the recipients at the same index
+     * @return feesInBPS List of fees express in bps to send to the recipients at the same index
      */
     function getRoyalties(address tokenAddress, uint256 tokenId)
         external
         view
-        returns (address payable[] memory recipients, uint256[] memory feeInBPS);
+        returns (address payable[] memory recipients, uint256[] memory feesInBPS);
 }
