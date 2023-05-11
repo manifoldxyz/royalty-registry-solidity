6a7cb798dc6518fca37f95cbee6a3d229c64eaa5
Kartik <slokhx@gmail.com>
2023-05-11 18:02:28 -0400

    Add deployment instructions and fix verification (#44)
    * Add script instructions
    
    * Fix verification api keys

--- deploy/deploy-engine.sh
+++ deploy/deploy-engine.sh
@@ -1,15 +1,36 @@
+# !/bin/bash
+
+# Usage:
+#   $ ./deploy/deploy-engine.sh
+#   $ ./deploy/deploy-engine.sh --broadcast
+#
+# Description:
+#   Deploys the Engine implementation and verifies the contract on relevant block explorer.
+#   The script will deploy the contract on all the networks specified in the `deployments` array.
+#   For ease of use, mainnets and testnets are separated into different arrays.
+#
+# Arguments:
+#   --broadcast - (optional) If specified, the script will broadcast the transaction to the network. 
+
+source .env
+
+# Mainnets
 deployments=(
     "1,mainnet,$ETHERSCAN_API_KEY"
-    "5,goerli,$ETHERSCAN_API_KEY"
     "137,polygon,$POLYGONSCAN_API_KEY"
-    "80001,polygon_mumbai,$POLYGONSCAN_API_KEY"
     "10,optimism,$OPSCAN_API_KEY"
-    "420,optimism_goerli,$OPSCAN_API_KEY"
-    "42161,arbitrum,$ARBISCAN_API_KEY"
-    "421613,arbitrum_goerli,$ARBISCAN_API_KEY"
+    "42161,arbitrum_one,$ARBISCAN_API_KEY"
     "43114,avalanche,$SNOWTRACE_API_KEY"
-    "43113,avalanche_fuji,$SNOWTRACE_API_KEY"
     "56,bnb_smart_chain,$BSCSCAN_API_KEY"
+)
+
+# Testnets
+deployments=(
+    "5,goerli,$ETHERSCAN_API_KEY"
+    "80001,polygon_mumbai,$POLYGONSCAN_API_KEY"
+    "420,optimism_goerli,$OPSCAN_API_KEY"
+    "421613,arbitrum_one_goerli,$ARBISCAN_API_KEY"
+    "43113,avalanche_fuji,$SNOWTRACE_API_KEY"
     "97,bnb_smart_chain_testnet,$BSCSCAN_API_KEY"
 )
 
@@ -32,10 +53,10 @@ for deployment in "${deployments[@]}"; do
         echo "[$chain_id] ========= Verifying the contract ========="
         forge verify-contract --watch \
             --chain $chain_id \
+            --constructor-args $(cast abi-encode "constructor(address)" $fallback_registry) \
             $engine_impl \
             contracts/RoyaltyEngineV1.sol:RoyaltyEngineV1 \
-            $api_key \
-            --constructor-args $(cast abi-encode "constructor(address)" $fallback_registry)
+            $api_key
     else
         echo "[$chain_id] ========= Skipping verification ========="
     fi
